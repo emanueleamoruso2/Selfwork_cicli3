bf8f45ebc7dd9cf1184c015d798a3dddd8201206
emanueleamoruso2 <emanuele.amoruso.a@gmail.com>
2026-03-10 13:42:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-let numero=Number(prompt('Scegli un numero da 1 a 3'));
-check=true
+let numero=prompt('Scegli un numero da 1 a 3');
 
 while(numero){
-if(numero !=1 && numero!=2 & numero!=3){
-numero=prompt('Risposta non valida. Scegli un numero da 1 a 3');
-}
-else if(numero==1){
+if(numero==1){
 console.log(`E' stata selezionata l'acqua`);
 break;
 }
@@ -16,5 +12,8 @@ break;
 else if(numero==3){
 console.log(`E' stata selezionata birra`);
 break;
+}
+else{
+numero=prompt('Risposta non valida. Scegli un numero da 1 a 3');
 }
 }
